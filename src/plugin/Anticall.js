40325df183b671f99f0011ee1dbd24eb8930c029
benmd981 <vnimsara492@gmail.com>
2024-11-03 23:07:47 +0530

    Update Anticall.js

--- src/plugin/Anticall.js
+++ src/plugin/Anticall.js
@@ -9,7 +9,7 @@ const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0
 const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === 'anticall') {
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*✨ THIS IS AN OWNER COMMAND*");
     let responseMessage;
 
     if (text === 'on') {
